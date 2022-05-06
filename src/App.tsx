dab08e1860283f4d7091f5680dd75a0ffd4117cc
robiel.tesfazghi@cpnv.ch <robiel.tesfazghi@cpnv.ch>
2022-05-06 10:15:09 +0200

    :zap: Update homepage to add Dog components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,29 @@ import React,{Component} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, ScrollView} from 'react-native';
 
+const Dog =() => {
+    return(
+        <View>
+            <Image
+          source={require('./assets/image/black.png')}
+          style={{ width: 300, height: 300 }}/>
+        <Image
+          source={require('./assets/image/logo.png')}
+          style={{ width: 300, height: 300 }}/>
+        <Image
+          source={require('./assets/image/beagle.jpg')}
+          style={{ width: 300, height: 200 }}/>
+        <Image
+          source={require('./assets/image/shibazu.jpg')}
+          style={{ width: 250, height: 300 }}/>
+        <Image
+          source={require('./assets/image/golden_retriever.jpg')}
+          style={{ width: 300, height: 200 }}/>
+        </View>
+        
+    );
+  }
+
 export default class App extends Component {
   render (){
     return (
@@ -13,21 +36,9 @@ export default class App extends Component {
         <Text style={{fontSize:15,padding:10,fontStyle:'italic'}}>
           Applications mobiles avec un frameworkhybride 🎉
         </Text>
-        <Image
-          source={require('./assets/image/black.png')}
-          style={{ width: 200, height: 200 }}/>
-        <Image
-          source={require('./assets/image/logo.png')}
-          style={{ width: 200, height: 200 }}/>
-        <Image
-          source={require('./assets/image/golden_retriever.jpg')}
-          style={{ width: 300, height: 200 }}/>
-        <Image
-          source={require('./assets/image/beagle.jpg')}
-          style={{ width: 300, height: 200 }}/>
-        <Image
-          source={require('./assets/image/shibazu.jpg')}
-          style={{ width: 200, height: 200 }}/>
+        <Dog />
+        <Dog />
+        <Dog />
         <StatusBar style="auto" />
       </View>
      </ScrollView>
