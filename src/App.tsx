65c73d23e7dd7ec2098cc5cc5e2526489a494b5f
robielcpnv <robiel.tesfazghi@cpnv.ch>
2022-05-06 08:50:16 +0200

    :zap: Update home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text
+      style={{fontSize:30,fontWeight:'bold',color:'#008080', padding:10}}
+      >MOB1</Text>
+      <Text
+       style={{fontSize:15,padding:10,fontStyle:'italic'}}
+      >
+        Applications mobiles avec un frameworkhybride 🎉
+        </Text>
+      <Image
+          source={require('./assets/image/black.png')}
+          style={{ width: 200, height: 200 }}
+        />
       <StatusBar style="auto" />
     </View>
   );
