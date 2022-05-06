32c462f0189d418b8c1c445b4b4012c670aa1fd0
robiel.tesfazghi@cpnv.ch <robiel.tesfazghi@cpnv.ch>
2022-05-06 09:37:16 +0200

    :dolphin:cosmetique

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, ScrollView} from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render (){
     return (
      <ScrollView>
