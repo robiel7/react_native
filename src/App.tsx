e9e0a4441e9e91c2d26d8b7577c9776d567b4573
robiel.tesfazghi@cpnv.ch <robiel.tesfazghi@cpnv.ch>
2022-05-06 09:15:12 +0200

    :zap: Update from function Component to Class Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text
-      style={{fontSize:30,fontWeight:'bold',color:'#008080', padding:10}}
-      >MOB1</Text>
-      <Text
-       style={{fontSize:15,padding:10,fontStyle:'italic'}}
-      >
-        Applications mobiles avec un frameworkhybride 🎉
-        </Text>
-      <Image
-          source={require('./assets/image/black.png')}
-          style={{ width: 200, height: 200 }}
-        />
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render (){
+    return (
+      <View style={styles.container}>
+        <Text
+        style={{fontSize:30,fontWeight:'bold',color:'#008080', padding:10}}
+        >MOB1</Text>
+        <Text
+         style={{fontSize:15,padding:10,fontStyle:'italic'}}
+        >
+          Applications mobiles avec un frameworkhybride 🎉
+          </Text>
+        <Image
+            source={require('./assets/image/black.png')}
+            style={{ width: 200, height: 200 }}
+          />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
